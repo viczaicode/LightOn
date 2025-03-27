522295f864f20d97914e24992ab82b8bba5ea42b
viczaicode <viczaimilan2003@gmail.com>
2025-03-27 09:51:52 +0100

    Lampa osztaly

---
+++ Lampa.js
@@ -0,0 +1,38 @@
+export class Lampa{
+    #allapot
+    #id
+    #szuloElem
+    constructor(id, allapot, szuloElem){
+        this.#id = id
+        this.#allapot = allapot
+        this.#szuloElem = szuloElem
+        this.#megjelenit();
+    }
+    #megjelenit(){
+        let txt = ""
+        if(this.#allapot === true){
+            txt += `<div id="${this.#id}" class="gombok1"></div>`
+        }else{
+            txt += `<div id="${this.#id}" class="gombok0"></div>`
+        }
+        this.#szuloElem.append(txt)
+        this.#szuloElem.find(`#${this.#id}`).on("click", () => this.#kattintasTrigger());
+    }
+    #setAllapot(){
+        this.#allapot = true;  // Csak akkor változtatjuk meg, ha sárga
+        this.#szinBeallit();
+    }
+    #szinBeallit(){
+        const elem = this.#szuloElem.find(`#${this.#id}`);
+        if(this.#allapot){
+            elem.removeClass("gombok0").addClass("gombok1");
+        } else {
+            elem.removeClass("gombok1").addClass("gombok0");
+        }
+    }
+    #kattintasTrigger(){
+        if(!this.#allapot){ // Csak akkor állítjuk be, ha most sárga (kikapcsolt)
+            this.#setAllapot();
+        }
+    }
+}
